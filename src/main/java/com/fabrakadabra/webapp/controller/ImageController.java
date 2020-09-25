8bdc63d51887bd5a84b7fd44d62ca0a032e60a88
daniboy01 <pappdaniel012@gmail.com>
2020-09-25 09:17:59 +0200

    updloadImage feature works fine

--- src/main/java/com/fabrakadabra/webapp/controller/ImageController.java
+++ src/main/java/com/fabrakadabra/webapp/controller/ImageController.java
@@ -2,9 +2,10 @@ package com.fabrakadabra.webapp.controller;
 
 import com.fabrakadabra.webapp.service.ImageService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -15,11 +16,11 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @PostMapping("/uploadImage")
-    public String uploadImage(@RequestBody MultipartFile imageFile){
+    @PostMapping("/uploadImage/{id}")
+    public String uploadImage(@RequestBody MultipartFile imageFile, @PathVariable Long id){
         String returnValue = "";
         try {
-            imageService.saveImage(imageFile);
+            imageService.saveImage(imageFile,id);
         }catch (Exception e){
            return "Image upload error";
         }
--- src/main/java/com/fabrakadabra/webapp/service/ImageService.java
+++ src/main/java/com/fabrakadabra/webapp/service/ImageService.java
@@ -1,19 +1,34 @@
 package com.fabrakadabra.webapp.service;
 
+import com.fabrakadabra.webapp.model.PlayGroundImg;
+import com.fabrakadabra.webapp.repository.PlayGroundImgRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
+@AllArgsConstructor
 public class ImageService  {
+    private final String folder = "src/img/";
+    private PlayGroundImgRepository playGroundImgRepository;
 
-    public void saveImage(MultipartFile imageFile) throws Exception {
-        String folder = "src/img/";
+    public void saveImage(MultipartFile imageFile,Long id) throws Exception {
         byte [] bytes = imageFile.getBytes();
         Path path = Paths.get(folder + imageFile.getOriginalFilename());
         Files.write(path,bytes);
+        imageToPlayGroundImage(imageFile.getOriginalFilename(),id);
+    }
+
+    public void imageToPlayGroundImage(String imageName,Long id){
+        File file = new File(folder + "/" + imageName);
+        String absolutePath = file.getAbsolutePath();
+        PlayGroundImg save = playGroundImgRepository.findById(id).get();
+        save.setURL(absolutePath);
+        playGroundImgRepository.save(save);
     }
 }
